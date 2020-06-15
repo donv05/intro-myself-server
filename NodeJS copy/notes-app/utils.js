29afd749f3a2093328946cc0cbe0a0a1ff6c41e2
donv05 <donv051994@gmail.com>
2020-06-15 12:57:00 +0700

    commit code

--- NodeJS copy/notes-app/utils.js
+++
@@ -1,6 +0,0 @@
-const name = "Do"
-const add = function(a, b) {
-    return a + b;
-}
-
-module.exports = add;
--- NodeJS copy/weather-app/public/js/app.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-const url = 'http://api.weatherstack.com/current?access_key=d8a59a2ddf42a084b86733e73c8f43a3&query=New%20York';
-fetch(url).then((respone) => {
-    respone.json().then((data) => {
-        console.log(data)
-    });
-})
