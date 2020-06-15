008136304dbd81780ec9379ec4d5b02c63499c99
donv05 <donv051994@gmail.com>
2020-06-15 20:27:37 +0700

    config evn

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.URL_DB, { useNewUrlParser: true, useUnifiedTopology: true });
