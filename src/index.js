7f2323f5f132e15ecadc729405a9a59d347e1e5d
donv05 <donv051994@gmail.com>
2020-06-15 23:15:57 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -21,16 +21,16 @@ app.use('/skills', skill)
 app.use('/auth', token)
 app.listen(process.env.PORT, () => console.log(`Example app listening at http://localhost:${process.env.PORT}`))
 
-const main = async () => {
-    // const task = await Task.findById('5ecf59edb350eb23e838f4ec')
-    // await task.populate('owner').execPopulate()
-    // console.log(task)
-    // User.find({}).populate({ path: 'task', select: '_id owner' }).exec(function(error, bands) {
-//     const  u = await User.findById('5ecdbd1ce849e06f2c98e9f0')
-//    await u.populate('tasks').execPopulate();
-//    console.log(u.tasks)
-//     User.findById('5ecdbd1ce849e06f2c98e9f0').populate({ path: 'tasks', select: '_id owner' }).exec(function(error, bands) {
-//        console.log(bands.tasks)
-//    })
-} 
-main()
+// const main = async () => {
+//     // const task = await Task.findById('5ecf59edb350eb23e838f4ec')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task)
+//     // User.find({}).populate({ path: 'task', select: '_id owner' }).exec(function(error, bands) {
+// //     const  u = await User.findById('5ecdbd1ce849e06f2c98e9f0')
+// //    await u.populate('tasks').execPopulate();
+// //    console.log(u.tasks)
+// //     User.findById('5ecdbd1ce849e06f2c98e9f0').populate({ path: 'tasks', select: '_id owner' }).exec(function(error, bands) {
+// //        console.log(bands.tasks)
+// //    })
+// } 
+// main()
